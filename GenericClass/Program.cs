d4a912165b073119f35e4f11c5209571bda64ad0
Kemalettin Aktaş <kemalettin.aktas@gazi.edu.tr>
2019-12-18 12:48:45 +0300

    Proje dosyası ekle.

---
+++ GenericClass/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericClass
+{
+    public class Liste<L>
+    {
+        private L deger;
+
+        public L Islem
+        {
+            get { return deger; }
+            set { deger = value; }
+        }
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Liste<string> stringNesne = new Liste<string>();
+            stringNesne.Islem = "Kemalettin AKTAŞ";
+            Console.WriteLine(stringNesne.Islem);
+            Liste<int> intNesne = new Liste<int>();
+            intNesne.Islem = 1987;
+            Console.WriteLine(intNesne.Islem);
+        }
+    }
+}
